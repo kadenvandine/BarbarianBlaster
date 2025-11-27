ccce8d63545ceb703f46b60e59c3c3c80d8007fb
Kaden VanDine <kaden@vandine.org>
2025-11-27 00:39:51 -0500

    Created two resources
    I created two standard resources, one transparent green, and one transparent red, and wrote code such that the ghost turret is supposed to take on these resources when hovering over a free space to indicate whether or not you have enough gold to purchase it. I also fixed the issue where it was necessary to select the turret button before each time you wanted to place one, now you can place them freely without the need to select the button each time.

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -7,6 +7,8 @@ extends Camera3D
 
 @export var turret_scene: PackedScene
 @export var turret_quick_scene: PackedScene
+@export var ghost_material_can_build: StandardMaterial3D
+@export var ghost_material_cannot_build: StandardMaterial3D
 var _ghost_turret: Node3D = null
 
 @onready var ray_cast_3d = $RayCast3D
@@ -30,47 +32,41 @@ func _process(delta):
 	
 	if not is_instance_valid(_ghost_turret):
 		return
-		
+	_ghost_turret.visible = false
+	var ghost_mesh = _ghost_turret.get_node_or_null("TurretBase")
+	if not ghost_mesh: return
 	var current_cost = 0
 	if selected_turret_type == "normal":
 		current_cost = turret_cost
 	elif selected_turret_type == "quick":
 		current_cost = quick_turret_cost
-	
-	var can_build = false
-	
-	if ray_cast_3d.is_colliding():
-		var collider = ray_cast_3d.get_collider()
-		if collider is GridMap:
-			var collision_point = ray_cast_3d.get_collision_point()
-			var cell = gridmap.local_to_map(collision_point)
+	if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider() is GridMap:
+		_ghost_turret.visible = true
+		
+		var collision_point = ray_cast_3d.get_collision_point()
+		var cell = gridmap.local_to_map(collision_point)
+		var tile_position = gridmap.map_to_local(cell)
+		_ghost_turret.global_position = tile_position
+		
+		var has_gold = bank.gold >= current_cost
+		var is_empty = gridmap.get_cell_item(cell) == 0
+		
+		if is_empty and has_gold:
+			ghost_mesh.material_override = ghost_material_can_build
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 			
-			if gridmap.get_cell_item(cell) == 0 and bank.gold >= current_cost:
-				can_build = true
-				_ghost_turret.visible = true
-				Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+			if Input.is_action_just_pressed("click"):
+				gridmap.set_cell_item(cell, 1)
+				bank.gold -= current_cost
 				
-				var tile_position = gridmap.map_to_local(cell)
-				_ghost_turret.global_position = tile_position
-				
-				if Input.is_action_just_pressed("click"):
-					gridmap.set_cell_item(cell, 1)
-					bank.gold -= current_cost
-					
-					if selected_turret_type == "normal":
-						turret_manager.build_turret(tile_position)
-					elif selected_turret_type == "quick":
-						turret_manager.build_turret_quick(tile_position)
-						
-					_ghost_turret.queue_free()
-					_ghost_turret = null
-					selected_turret_type = ""
-					can_build = false
-					
-	if not can_build and is_instance_valid(_ghost_turret):
-		_ghost_turret.visible = false
-					
-					
+				if selected_turret_type == "normal":
+					turret_manager.build_turret(tile_position)
+				elif selected_turret_type == "quick":
+					turret_manager.build_turret_quick(tile_position)
+		
+		else:
+			ghost_mesh.material_override = ghost_material_cannot_build
+
 func _on_build_turret_pressed():
 	selected_turret_type = "normal"
 	spawn_ghost_turret(turret_scene)
@@ -82,7 +78,13 @@ func spawn_ghost_turret(scene: PackedScene):
 		_ghost_turret.queue_free()
 	if scene:
 		_ghost_turret = scene.instantiate()
+		var mesh = _ghost_turret.get_node_or_null("TurretBase")
+		if mesh and mesh is MeshInstance3D:
+			mesh.material_override = ghost_material_cannot_build
+		else:
+			print("Cannot find 'TurretBase mesh node in ghost turret!")
 		turret_manager.add_child(_ghost_turret)
+		_ghost_turret.visible = false
 					
 #		if bank.gold >= turret_cost:
 #			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
