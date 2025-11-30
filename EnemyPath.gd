7ccf65a94db8c24cb9f40d663348afd7712aef51
Kaden VanDine <kaden@vandine.org>
2025-11-29 19:46:34 -0500

    Tutorial trigger
    Updated EnemyPath.gd in accordance to GameSettings.gd such that the tutorial will begin as the first barbarians spawn.

--- EnemyPath.gd
+++ EnemyPath.gd
@@ -3,9 +3,17 @@ extends Path3D
 @export var enemy_scene: PackedScene
 @export var difficulty_manager: Node
 @export var victory_layer: CanvasLayer
+@export var tutorial_layer: CanvasLayer
+
 @onready var timer = $Timer
 
+var _first_enemy_spawned := false
+
 func spawn_enemy() -> void:
+	if not _first_enemy_spawned:
+		_first_enemy_spawned = true
+		if is_instance_valid(tutorial_layer) and GameSettings.show_tutorial:
+			tutorial_layer.start_tutorial()
 	var new_enemy = enemy_scene.instantiate()
 	var current_health = difficulty_manager.get_enemy_health()
 	var current_speed = difficulty_manager.get_enemy_speed()
